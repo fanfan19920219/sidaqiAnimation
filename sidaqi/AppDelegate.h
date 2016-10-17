f72536496bcc14e50c2b020cc27befc6700ed60f
zhangzhihua19920219 <406608969@qq.com>
2016-10-17 13:07:46 +0800

    Initial Commit

---
+++ sidaqi/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  sidaqi
+//
+//  Created by M-SJ077 on 2016/10/17.
+//  Copyright © 2016年 zhangzhihua. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ sidaqi/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  sidaqi
+//
+//  Created by M-SJ077 on 2016/10/17.
+//  Copyright © 2016年 zhangzhihua. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
